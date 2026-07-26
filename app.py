a084cb51d8d0125b183fa28a834008b128ad252f
rebeka814 <agrerebecca1@gmail.com>
2026-07-26 17:01:39 +0000

    changer type de graphe pour pouvoir zoomer

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import tempfile
 import cv2
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 from src.processing import extract_signal 
 from src.signal_analysis import get_time_axis, analyze_frequency 
 
@@ -24,25 +25,30 @@ if uploaded_file is not None:
     if ret:
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         cv2.rectangle(frame_rgb, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        st.image(frame_rgb, caption="Preview of the calculating zon (Red zone)")
+        st.image(frame_rgb, caption="Preview of the calculating zone (red zone)")
         intensity, fps = extract_signal(temp_video_path, x, y, w, h)
         time_axis = get_time_axis(intensity, fps)
         peaks, period, freq, smoothed_intensity = analyze_frequency(intensity, fps)
         col1, col2  = st.columns(2)
-        col1.metric("Frequency (Hz)", f"{freq:.2f}")
-        col2.metric("Period (s)", f"{period:.2f}")
-        fig, ax = plt.subplots(figsize=(8, 4))
-        ax.plot(time_axis, intensity, label="Intensity", color="blue")
+        col1.metric("Frequency (Hz)", f"{freq:.5f}")
+        col2.metric("Period (s)", f"{period:.5f}")
+        #fig, ax = plt.subplots(figsize=(8, 4))
+        #ax.plot(time_axis, intensity, label="Intensity", color="blue")
+        #if len(peaks) > 0:
+        #    ax.plot(time_axis[peaks], intensity[peaks], "ro", label="Detected peaks")
+        #ax.set_xlabel("Time (s)")
+        #ax.set_ylabel("Mean intensity")
+        #ax.set_title("Hydrogel pulse signal")
+        #ax.grid(True)
+        #ax.legend()
+        #st.pyplot(fig)
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=time_axis, y=intensity, mode="lines", name="Intensity", line=dict(color="blue")))
         if len(peaks) > 0:
-            ax.plot(time_axis[peaks], intensity[peaks], "ro", label="Detected peaks")
-        ax.set_xlabel("Time (s)")
-        ax.set_ylabel("Mean intensity")
-        ax.set_title("Hydrogel pulse signal")
-        ax.grid(True)
-        ax.legend()
-        st.pyplot(fig)
+            fig.add_trace(go.Scatter(x=time_axis[peaks], y=intensity[peaks], mode="markers", name="Detected peaks", marker=dict(color="red", size=8)))
+        fig.update_layout(title="Hydrogel pulse signal",xaxis_title="Time (s)", yaxis_title="Mean intensity", hovermode="x unified")
+        st.plotly_chart(fig, use_container_width=True)
 
 
-
-st.success("Success message")
+st.success("Success")
 
