a85111ad895ce197086fac6b6fbdfd5d68f34bee
rebeka814 <agrerebecca1@gmail.com>
2026-07-27 14:01:21 +0000

    explication code app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import tempfile
 import cv2
-#import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from src.processing import extract_signal 
 from src.signal_analysis import get_time_axis, analyze_frequency 
@@ -16,39 +15,28 @@ h = st.sidebar.number_input("height", value=20)
 
 uploaded_file = st.sidebar.file_uploader("Hydrogel video", type=["avi", "mp4"])
 if uploaded_file is not None:
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.avi') as tfile:
-        tfile.write(uploaded_file.read())
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.avi') as tfile: #create a temporary file named tfile
+        tfile.write(uploaded_file.read()) #store the uploaded video in tfile
         temp_video_path = tfile.name
-    cap = cv2.VideoCapture(temp_video_path)
-    ret, frame = cap.read()
-    cap.release()
+    cap = cv2.VideoCapture(temp_video_path) #open the video file
+    ret, frame = cap.read() #read the first frame and store it in 'frame'
+    cap.release() #close the video
     if ret:
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        cv2.rectangle(frame_rgb, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        st.image(frame_rgb, caption="Preview of the calculating zone (red zone)")
+        cv2.rectangle(frame_rgb, (x, y), (x + w, y + h), (255, 0, 0), 2) 
+        st.image(frame_rgb, caption="Preview of the calculating zone (red zone)") #dispay the image 'frame'
         intensity, fps = extract_signal(temp_video_path, x, y, w, h)
         time_axis = get_time_axis(intensity, fps)
         peaks, period, freq, smoothed_intensity = analyze_frequency(intensity, fps)
         col1, col2  = st.columns(2)
-        col1.metric("Frequency (Hz)", f"{freq:.5f}")
+        col1.metric("Frequency (Hz)", f"{freq:.5f}") #display metric
         col2.metric("Period (s)", f"{period:.5f}")
-        #fig, ax = plt.subplots(figsize=(8, 4))
-        #ax.plot(time_axis, intensity, label="Intensity", color="blue")
-        #if len(peaks) > 0:
-        #    ax.plot(time_axis[peaks], intensity[peaks], "ro", label="Detected peaks")
-        #ax.set_xlabel("Time (s)")
-        #ax.set_ylabel("Mean intensity")
-        #ax.set_title("Hydrogel pulse signal")
-        #ax.grid(True)
-        #ax.legend()
-        #st.pyplot(fig)
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=time_axis, y=intensity, mode="lines", name="Intensity", line=dict(color="blue")))
+        fig = go.Figure() #initialize a figure
+        fig.add_trace(go.Scatter(x=time_axis, y=intensity, mode="lines", name="Intensity", line=dict(color="blue", dash="dash")))
         if len(peaks) > 0:
             fig.add_trace(go.Scatter(x=time_axis[peaks], y=intensity[peaks], mode="markers", name="Detected peaks", marker=dict(color="red", size=8)))
         fig.update_layout(title="Hydrogel pulse signal",xaxis_title="Time (s)", yaxis_title="Mean intensity", hovermode="x unified")
-        st.plotly_chart(fig, use_container_width=True)
-
+        st.plotly_chart(fig, use_container_width=True) #displays the plot
 
 st.success("Success")
 
