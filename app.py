7ea90694055e7c4bf66fc5b11fa9ceabb45003a5
rebeka814 <agrerebecca1@gmail.com>
2026-07-29 17:00:53 +0000

    visualisation toutes les frames

--- app.py
+++ app.py
@@ -24,12 +24,15 @@ if uploaded_file is not None:
     cap = cv2.VideoCapture(temp_video_path) #open the video file
     video_width, video_height = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     video_shape = (video_width, video_height)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frame_idx = st.sidebar.slider("Preview frame index", 0, total_frames - 1, 0)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
     ret, frame = cap.read() #read the first frame and store it in 'frame'
     cap.release() #close the video
     if ret:
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         cv2.rectangle(frame_rgb, (x, y), (x + w, y + h), (0, 255, 0), 2) 
-        st.image(frame_rgb, caption=f"Preview of the calculating zone (green zone). \n Picture size: {video_shape}") #dispay the image 'frame'
+        st.image(frame_rgb, caption=f"Preview of the calculating zone (green zone) - Frame {frame_idx}/{total_frames} - Picture size: {video_shape}") #dispay the image 'frame'
 
         intensity, fps = extract_signal(temp_video_path, x, y, w, h)
         time_axis = get_time_axis(intensity, fps)
